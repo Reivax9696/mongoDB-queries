f1459cb81964515136036129974eac667526769a
Reivax9696 <149475827+Reivax9696@users.noreply.github.com>
2024-01-11 02:48:28 +0100

    Update mongoqueries.js

--- mongoqueries.js
+++ mongoqueries.js
@@ -8,7 +8,7 @@
 8- db.restaurants.find({ "grades.score": { $gt: 90 } })
 9- db.restaurants.find({ "grades.score": { $gt: 80, $lt: 100 } })
 10- db.restaurants.find({ "address.coord.0": { $lt: -95.754168 } })
-11- db.restaurants.find({"cuisine": { $ne: "American" },"grades.score": { $gt: 70 }, "address.coord.1": { $lt: -65.754168 }})
+11- db.restaurants.find({$and: [{ "cuisine": { $ne: "American" } }, { "grades.score": { $gt: 70 } },{ "address.coord.1": { $lt: -65.754168}}]})
 12- db.restaurants.find({"cuisine": { $ne: "American" }, "grades.score": { $gt: 70 }, "address.coord.0": { $lt: -65.754168 } })
 13- db.restaurants.find({"cuisine": { $ne: "American" }, "borough": { $ne: "Brooklyn" }, "grades.grade": "A"}).sort({ "cuisine": -1 })
 14- db.restaurants.find({ "name": /^Wil/ }, { "restaurant_id": 1, "name": 1, "borough": 1, "cuisine": 1})
